028b7090152ca5b10a679b3a8635a74be518aa02
Cheuk Shu Ho <21237387@life.hkbu.edu.hk>
2022-10-01 17:43:38 +0800

    Initial commit

---
+++ Component/Footer.php
@@ -0,0 +1,149 @@
+<!doctype html>
+<html lang="en">
+<head>
+<!-- Boostrap5 -->   
+<!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+<!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+
+<!-- MDB -->
+<!-- Font Awesome -->
+<link
+  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
+  rel="stylesheet"
+/>
+<!-- Google Fonts -->
+<link
+  href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+  rel="stylesheet"
+/>
+<!-- MDB -->
+<link
+  href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/5.0.0/mdb.min.css"
+  rel="stylesheet"
+/>
+<!-- MDB -->
+<script
+  type="text/javascript"
+  src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/5.0.0/mdb.min.js"
+></script>
+</head>
+
+<body>
+
+<footer class="container py-5">
+  <div class="row">
+    <div class="col-12 col-md">
+      <img src="../img/CareHK.jpg" width="24" height="24" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="d-block mb-2" role="img" viewBox="0 0 24 24"><title>Product</title><circle cx="12" cy="12" r="10"/><path d="M14.31 8l5.74 9.94M9.69 8h11.48M7.38 12l5.74-9.94M9.69 16L3.95 6.06M14.31 16H2.83m13.79-4l-5.74 9.94"/></svg>
+      <small class="d-block mb-3 text-muted">&copy; 2022–2023</small>
+    </div>
+    <div class="col-6 col-md">
+      <h5>About CareHK</h5>
+      <ul class="list-unstyled text-small">
+        <li><a class="link-secondary" href="#">Cool stuff</a></li>
+        <li><a class="link-secondary" href="#">Random feature</a></li>
+        <li><a class="link-secondary" href="#">Team feature</a></li>
+        <li><a class="link-secondary" href="#">Stuff for developers</a></li>
+        <li><a class="link-secondary" href="#">Another one</a></li>
+        <li><a class="link-secondary" href="#">Last time</a></li>
+      </ul>
+    </div>
+    <div class="col-6 col-md">
+      <h5>Get help</h5>
+      <ul class="list-unstyled text-small">
+        <li><a class="link-secondary" href="#">Resource name</a></li>
+        <li><a class="link-secondary" href="#">Resource</a></li>
+        <li><a class="link-secondary" href="#">Another resource</a></li>
+        <li><a class="link-secondary" href="#">Final resource</a></li>
+      </ul>
+    </div>
+    <div class="col-6 col-md">
+      <h5>Discover</h5>
+      <ul class="list-unstyled text-small">
+        <li><a class="link-secondary" href="#">Business</a></li>
+        <li><a class="link-secondary" href="#">Education</a></li>
+        <li><a class="link-secondary" href="#">Government</a></li>
+        <li><a class="link-secondary" href="#">Gaming</a></li>
+      </ul>
+    </div>
+    <div class="col-6 col-md">
+      <h5>About</h5>
+<!-- Facebook -->
+<a class="btn btn-primary btn-lg btn-floating" style="background-color: #3b5998;" href="#!" role="button"
+  ><i class="fab fa-facebook-f"></i
+></a>
+
+<!-- Twitter -->
+<a class="btn btn-primary btn-lg btn-floating" style="background-color: #55acee;" href="#!" role="button"
+  ><i class="fab fa-twitter"></i
+></a>
+
+<!-- Google -->
+<a class="btn btn-primary btn-lg btn-floating" style="background-color: #dd4b39;" href="#!" role="button"
+  ><i class="fab fa-google"></i
+></a>
+
+<!-- Instagram -->
+<a class="btn btn-primary btn-lg btn-floating" style="background-color: #ac2bac;" href="#!" role="button"
+  ><i class="fab fa-instagram"></i
+></a>
+
+<!-- Linkedin -->
+<a class="btn btn-primary btn-lg btn-floating" style="background-color: #0082ca;" href="#!" role="button"
+  ><i class="fab fa-linkedin-in"></i
+></a>
+
+<!-- Pinterest -->
+<a class="btn btn-primary btn-lg btn-floating" style="background-color: #c61118;" href="#!" role="button"
+  ><i class="fab fa-pinterest"></i
+></a>
+
+<!-- Vkontakte -->
+<a class="btn btn-primary btn-lg btn-floating" style="background-color: #4c75a3;" href="#!" role="button"
+  ><i class="fab fa-vk"></i
+></a>
+
+<!-- Stack overflow -->
+<a class="btn btn-primary btn-lg btn-floating" style="background-color: #ffac44;" href="#!" role="button"
+  ><i class="fab fa-stack-overflow"></i
+></a>
+
+<!-- Youtube -->
+<a class="btn btn-primary btn-lg btn-floating" style="background-color: #ed302f;" href="#!" role="button"
+  ><i class="fab fa-youtube"></i
+></a>
+
+<!-- Slack -->
+<a class="btn btn-primary btn-lg btn-floating" style="background-color: #481449;" href="#!" role="button"
+  ><i class="fab fa-slack-hash"></i
+></a>
+
+<!-- Github -->
+<a class="btn btn-primary btn-lg btn-floating" style="background-color: #333333;" href="#!" role="button"
+  ><i class="fab fa-github"></i
+></a>
+
+<!-- Dribbble -->
+<a class="btn btn-primary btn-lg btn-floating" style="background-color: #ec4a89;" href="#!" role="button"
+  ><i class="fab fa-dribbble"></i
+></a>
+
+<!-- Reddit -->
+<a class="btn btn-primary btn-lg btn-floating" style="background-color: #ff4500;" href="#!" role="button"
+  ><i class="fab fa-reddit-alien"></i
+></a>
+
+<!-- Whatsapp -->
+<a class="btn btn-primary btn-lg btn-floating" style="background-color: #25d366;" href="#!" role="button"
+  ><i class="fab fa-whatsapp"></i
+></a>
+    </div>
+  </div>
+</footer>
+
+</body>
+
+
+
+</html>
---
+++ Component/Navbar.php
@@ -0,0 +1,46 @@
+<!doctype html>
+<html lang="en">
+<head>
+    
+<!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+<!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+
+</head>
+
+<body>
+<nav class="navbar navbar-expand-lg bg-danger">
+  <div class="container-fluid">
+  <a class="navbar-brand" href="#">
+      <img src="../img/CareHK.jpg" alt="Logo" width="30" height="24" class="d-inline-block align-text-top">
+      CareHK
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+
+
+      </ul>
+      <form class="d-flex" role="search">
+        <button class="btn btn-primary" type="button">Login</button>
+        &nbsp;
+        <button class="btn btn-outline-warning" type="button">Sign Up</button>
+      </form>
+    </div>
+  </div>
+</nav>
+
+</body>
+
+
+
+</html>
